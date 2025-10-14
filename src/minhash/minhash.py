959abdc0c083f2f77998ed4797dac1bfeb0201ad
Hubert Dang <hubertdang@gmail.com>
2025-10-13 23:41:45 -0400

    Create constructor for a vanilla MinHash class

---
+++ src/minhash/minhash.py
@@ -0,0 +1,24 @@
+import mmh3
+
+k = 128
+
+# k hash functions that are uniformly at random (UAR)
+h = [lambda x, seed=i: mmh3.hash(str(x), seed=seed) for i in range(k)]
+
+
+def H(A: set, h_i) -> list:
+    """Applies a hash function h_i to all elements a of set A"""
+    return [h_i(a) for a in A]
+
+
+class VanillaMinHash:
+    """A k-MinHash sketch"""
+
+    def __init__(self, A: set):
+        if not isinstance(A, set):
+            raise TypeError("a must be of type 'set'")
+
+        self.signature = []
+
+        for i in range(k):
+            self.signature.append(min(H(A, h[i])))
